82bceaa43acdd6f2f925dcc0feb49f48ff705dd4
Shaun Warman <swarman@paypal.com>
2016-05-02 23:48:24 -0500

    Add back vanilla react logic

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from '../../common/store/configureStore';
-import App from '../../common/containers/App';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
 
-const initialState = window.__INITIAL_STATE__;
-const store = configureStore(initialState);
-const rootElement = document.getElementById('app');
-
-render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    rootElement
-);
+ReactDOM.render(Routes, document.getElementById('root'));
--- src/client/js/routes.js
+++ src/client/js/routes.js
@@ -3,7 +3,6 @@ import { Router, Route, IndexRoute } from 'react-router';
 import createHistory from 'history/lib/createHashHistory'
 
 import Home from './home';
-import LineChart from './charts/LineChart';
 
 const historyOptions = {
     queryKey : false
@@ -12,7 +11,6 @@ const historyOptions = {
 const routes = (
     <Router history={createHistory(historyOptions)}>
         <Route path='/' component={ Home }>
-            <Route path='linechart' component={ LineChart } />
         </Route>
     </Router>
 );
