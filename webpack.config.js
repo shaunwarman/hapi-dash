7fce06ee52e979db1333f70bcbade808402a877c
Shaun Warman <swarman@paypal.com>
2016-05-02 23:49:24 -0500

    Minor formatting

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@ var path = require('path');
 
 module.exports = {
     devtool: 'inline-source-map',
-    entry: './src/client/js/app.js'
-    ,
+    entry: './src/client/js/app.js',
     output: {
         path: path.join(__dirname, 'build'),
         filename: 'main.js'
